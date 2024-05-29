f7d2f377ac2b954d8f8836f03a23acf512aa2f55
Esraa <esraa.ramadan667@gmail.com>
2024-05-29 19:08:15 +0300

    23-5

--- lib/cubit/auth/auth_cubit.dart
+++ lib/cubit/auth/auth_cubit.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -8,48 +7,49 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:meta/meta.dart';
-
 import '../../model/user_model.dart';
-
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthInitial());
-static  AuthCubit get(context)=> BlocProvider.of(context);
+  static AuthCubit get(context) => BlocProvider.of(context);
 
-FirebaseAuth auth = FirebaseAuth.instance;
-FirebaseFirestore store = FirebaseFirestore.instance;
-UserModel userModel =UserModel();
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore store = FirebaseFirestore.instance;
+  UserModel userModel = UserModel();
 // register email& password
-  GoogleSignIn googleSignIn =GoogleSignIn();
+  GoogleSignIn googleSignIn = GoogleSignIn();
   final ImagePicker picker = ImagePicker();
- FirebaseStorage storage = FirebaseStorage.instance;
- late XFile image;
+  FirebaseStorage storage = FirebaseStorage.instance;
+  late XFile image;
 
-
-  registerByEmailAndPassword({ required String email,
-     required String password, String? name}) async {
-    try{
-    UserCredential credential =
-    await auth.createUserWithEmailAndPassword(
-        email: email, password: password);
-    emit(AuthRegisterByEmailState());
-    userModel.name= name;
-    userModel.email=email;
-    userModel.password =password;
-    userModel.id= credential.user!.uid;
-    await storage.ref().child("images/").child("${userModel.id}.png}")
-        .putFile(File(userImage!.path));
-    userModel.pic=  await storage.ref().child("images/").child("${userModel.id}.png}")
-        .getDownloadURL();
-    await store.collection("Users").doc(userModel.id).
-    set(userModel.toMap());
-    emit(RegisterByEmailAndPasswordSaveState());}
-    on FirebaseAuthException catch  (e) {
-           print('Failed with error code: ${e.code}');
-           print(e.message);
+  registerByEmailAndPassword(
+      {required String email, required String password, String? name}) async {
+    try {
+      UserCredential credential = await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      emit(AuthRegisterByEmailState());
+      userModel.name = name;
+      userModel.email = email;
+      userModel.password = password;
+      userModel.id = credential.user!.uid;
+      await storage
+          .ref()
+          .child("images/")
+          .child("${userModel.id}.png}")
+          .putFile(File(userImage!.path));
+      userModel.pic = await storage
+          .ref()
+          .child("images/")
+          .child("${userModel.id}.png}")
+          .getDownloadURL();
+      await store.collection("Users").doc(userModel.id).set(userModel.toMap());
+      emit(RegisterByEmailAndPasswordSaveState());
+    } on FirebaseAuthException catch (e) {
+      print('Failed with error code: ${e.code}');
+      print(e.message);
+    }
   }
-}
 
   // Future<void> registerByEmailAndPassword({
   //   required String email,
@@ -89,7 +89,7 @@ UserModel userModel =UserModel();
   //   }
   // }
 
- // isAdmin  "register"
+  // isAdmin  "register"
   // Future<void> registerByEmailAndPassword({
   //   required String email,
   //   required String password,
@@ -130,13 +130,15 @@ UserModel userModel =UserModel();
   //     print(e.message);
   //   }
   // }
-loginByEmailAndPassword({required String email,required String password })async{
- UserCredential userLogin=  await auth.signInWithEmailAndPassword(email: email, password: password);
- var userData=    await store.collection("profile").doc(userModel.id).get();
- userModel =UserModel.fromMap(userData.data()!);
- //GetallUser
- emit(AuthRegisterByEmailState());
-}
+  loginByEmailAndPassword(
+      {required String email, required String password}) async {
+    UserCredential userLogin =
+        await auth.signInWithEmailAndPassword(email: email, password: password);
+    var userData = await store.collection("profile").doc(userModel.id).get();
+    userModel = UserModel.fromMap(userData.data()!);
+    //GetallUser
+    emit(AuthRegisterByEmailState());
+  }
 // https://firebase.flutter.dev/docs/auth/phone
 
 // isAdmin "login"
@@ -175,48 +177,51 @@ loginByEmailAndPassword({required String email,required String password })async{
 //   }
   /////////// ristrict
 
-  registerByGoogle ()async{
- await googleSignIn.signOut();
-  emit(AuthLoadingState());
-  GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
-  GoogleSignInAuthentication? googleSignInAuthentication = await
-  googleSignInAccount!
-      .authentication;
-  AuthCredential userCredential = GoogleAuthProvider.credential(
-    idToken: googleSignInAuthentication.idToken,
-    accessToken: googleSignInAuthentication.accessToken,
-  );
-  UserCredential userByGoogle= await auth.signInWithCredential(userCredential);
-  //////////////
-  userModel.id = userByGoogle.user!.uid; //orpRW53##
-  userModel.name = userByGoogle.user!.displayName;
-  userModel.pic =userByGoogle.user!.photoURL;
-  userModel.email = userByGoogle.user!.email;
-  await store.collection("profile").doc(userModel.id).
-  set(userModel.toMap());
-  emit(AuthRegisterByGoogleState());
-}
-XFile? userImage;
-  uploadImage(String camera)async{
-    if(camera == "cam"){
-      userImage = (await picker.pickImage(source: ImageSource.camera))!;
-      await storage.ref().child('images/').child("${userModel.id} as camera.png")
-          .putFile(File(userImage!.path));
-      emit(UploadPhotoState());
-      return userImage?.readAsBytes();
-    }
-    else
-    {
-      userImage = (await picker.pickImage(source: ImageSource.gallery))!;
-      await storage.ref().child('images/').child("${userModel.id}as gallery")
-          .putFile(File(userImage!.path));
-      emit(UploadPhotoState());
-      return userImage?.readAsBytes();
-    }
-
+  registerByGoogle() async {
+    await googleSignIn.signOut();
+    emit(AuthLoadingState());
+    GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+    GoogleSignInAuthentication? googleSignInAuthentication =
+        await googleSignInAccount!.authentication;
+    AuthCredential userCredential = GoogleAuthProvider.credential(
+      idToken: googleSignInAuthentication.idToken,
+      accessToken: googleSignInAuthentication.accessToken,
+    );
+    UserCredential userByGoogle =
+        await auth.signInWithCredential(userCredential);
+    //////////////
+    userModel.id = userByGoogle.user!.uid; //orpRW53##
+    userModel.name = userByGoogle.user!.displayName;
+    userModel.pic = userByGoogle.user!.photoURL;
+    userModel.email = userByGoogle.user!.email;
+    await store.collection("profile").doc(userModel.id).set(userModel.toMap());
+    emit(AuthRegisterByGoogleState());
   }
 
-List users =[];
+  XFile? userImage;
+  uploadImage(String camera) async {
+    if (camera == "cam") {
+      userImage = (await picker.pickImage(source: ImageSource.camera))!;
+      await storage
+          .ref()
+          .child('images/')
+          .child("${userModel.id} as camera.png")
+          .putFile(File(userImage!.path));
+      emit(UploadPhotoState());
+      return userImage?.readAsBytes();
+    } else {
+      userImage = (await picker.pickImage(source: ImageSource.gallery))!;
+      await storage
+          .ref()
+          .child('images/')
+          .child("${userModel.id}as gallery")
+          .putFile(File(userImage!.path));
+      emit(UploadPhotoState());
+      return userImage?.readAsBytes();
+    }
+  }
+
+  List users = [];
   getUsers() async {
     try {
       var fireStoreUsers = await store
